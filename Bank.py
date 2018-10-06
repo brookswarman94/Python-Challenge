ce4854a2f4fd15098cf869236d4291f54af07b60
brookswarman94 <43188511+brookswarman94@users.noreply.github.com>
2018-10-06 10:55:07 -0700

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,123 @@
+import os
+import csv
+
+
+
+data_budget = os.path.join('downloads','budget_data.csv')
+
+
+
+
+# Variables to Track
+
+total_months = 0
+
+total_revenue = 0
+
+
+
+prev_revenue = 0
+
+revenue_change = 0
+
+greatest_increase = ["", 0]
+
+greatest_decrease = ["", 9999999999999999999999]
+
+
+
+revenue_changes = []
+
+
+
+# Read Files
+
+with open(data_budget) as revenue_data:
+
+    reader = csv.DictReader(revenue_data)
+
+
+
+
+
+    for row in reader:
+
+
+
+        
+
+        total_months = total_months + 1
+
+        total_revenue = total_revenue + int(row["Profit/Losses"])
+
+        # print(row)
+
+
+
+        # Keep track of changes
+
+        revenue_change = int(row["Profit/Losses"]) - prev_revenue
+
+        # print(revenue_change)
+
+
+
+        # Reset the value of prev_revenue to the row I completed my analysis
+
+        prev_revenue = int(row["Profit/Losses"])
+
+        # print(prev_revenue)
+
+
+
+        # Determine the greatest increase
+
+        if (revenue_change > greatest_increase[1]):
+
+            greatest_increase[1] = revenue_change
+
+            greatest_increase[0] = row["Date"]
+
+
+
+        if (revenue_change < greatest_decrease[1]):
+
+            greatest_decrease[1] = revenue_change
+
+            greatest_decrease[0] = row["Date"]
+
+
+
+        # Add to the revenue_changes list
+
+        revenue_changes.append(int(row["Profit/Losses"]))
+
+
+
+    # Set the Revenue average
+
+    revenue_avg = sum(revenue_changes) / len(revenue_changes)
+
+    
+
+    # Show Output
+
+    print()
+
+    print()
+
+    print()
+
+    print("Financial Analysis")
+
+    print("-------------------------")
+
+    print("Total Months: " + str(total_months))
+
+    print("Total Revenue: " + "$" + str(total_revenue))
+
+    print("Average Change: " + "$" + str(round(sum(revenue_changes) / (total_months))))
+
+    print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
+
+    print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
---
+++ Polls.py
@@ -0,0 +1,121 @@
+import csv
+
+import os
+
+from pathlib import Path
+
+
+
+#set path
+
+filepath = Path("downloads\election_data.csv")
+
+print('Election Results\n')
+
+print('---------------------------\n')
+
+#open the file
+
+with open(filepath, newline="",encoding='utf-8') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    #Initialize variables to empty lists and dictionary
+
+    votes = []
+
+    total_votes = 0
+
+    candidates = []
+
+    candidates_dict = {}
+
+    winning_votes = 0
+
+    next(csvreader)
+
+    #go line by line and process each vote
+
+    for row in csvreader:
+
+        #add to total number of votes
+
+        total_votes += 1
+
+        if row[2] not in candidates:
+
+            #add candidates to the empty list
+
+            candidates.append(row[2])
+
+        #to make candidate name as the key
+
+            candidates_dict[row[2]] = 0
+
+            candidates_dict[row[2]] = candidates_dict[row[2]] + 1
+
+        else:     
+
+            candidates_dict[row[2]] +=1
+
+    #print(candidates_dict)
+
+    for candidates in candidates_dict:
+
+        votes = candidates_dict.get(candidates)
+
+        percentage = (votes / total_votes)*100
+
+        print(str(candidates) + ':' + str(round(percentage,3)) + ("%") + '   (' + str(votes) + ')' '\n')
+
+        if votes > winning_votes:
+
+            winning_votes = votes
+
+            winner = candidates
+
+            
+
+print("Total Votes: " + str(total_votes) + '\n')
+
+print('----------------------------\n')
+
+print("Winner: " + str(winner) +'\n')
+
+print("Winning Votes: " + str(winning_votes) +'\n')
+
+print('----------------------------\n')
+
+
+
+#set path for output file
+
+filepath_1 = ("downloads\output_pypoll.txt")
+
+# opens the output destination in write mode and prints the summary
+
+with open(filepath_1, 'w+') as writefile:
+
+    csvwriter = csv.writer(writefile, delimiter=",")
+
+    writefile.writelines('Election Results\n')
+
+    writefile.writelines('----------------------------\n')
+
+    for candidates in candidates_dict:
+
+        votes = candidates_dict.get(candidates)
+
+        percentage = (votes / total_votes)*100
+
+        writefile.writelines(str(candidates) + ':' + str(round(percentage,3)) + ("%") + '   (' + str(votes) + ')' '\n')
+
+    writefile.writelines("Total Votes: " + str(total_votes) + '\n')
+
+    writefile.writelines('----------------------------\n')
+
+    writefile.writelines("Winner: " + str(winner) +'\n')
+
+    writefile.writelines("Winning Votes: " + str(winning_votes) +'\n')
+
+    writefile.writelines('----------------------------\n')
